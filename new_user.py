ecf3832ff84820f858a1389f1a65a7764c800539
DenisIvanovo <78604494+DenisIvanovo@users.noreply.github.com>
2021-02-05 19:03:15 +0300

    Add files via upload

---
+++ new_user.py
@@ -0,0 +1,37 @@
+from app import db, User
+import os
+from werkzeug.security import generate_password_hash
+
+
+class New_user:
+    def __init__(self, form):
+        # Получаем данные из формы.
+        user_name = form.user_name.data
+        user_last_name = form.user_last_name.data
+        user_nic = form.user_nic.data
+        email = form.email.data
+        password = form.password.data
+        password_2 = form.password_2.data
+
+        # Хэшируем пароль пользователя.
+        password = generate_password_hash(password)
+        # Добавляем пользователвля в базу данных.
+        user_info = User(name=user_name,
+                         last_name=user_last_name,
+                         user_nic=user_nic,
+                         email=email,
+                         password=password)
+        try:
+            db.session.add(user_info)
+            db.session.commit()
+            print('Пользователь добавлен в базу')
+        except ValueError:
+            print('error')
+        try:
+            # Создаем отдельную папку для загрузки фото
+            os.chdir('./static/')  # Переходи в нужную директорию.
+            # Создаем папку, для имени папки используем ник и email пользователя.
+            # ник и email берем из формы.
+            os.mkdir(f"{user_nic}-{email}")
+        except OSError:
+            pass
